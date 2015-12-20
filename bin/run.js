6412a0f2ce6501a206087379146158fb27aa9711
John Hannagan <weetbix_@hotmail.com>
2015-12-20 19:31:41 +0800

    Throw error when API_keys not set

--- bin/run.js
+++ bin/run.js
@@ -2,8 +2,12 @@
 
 var Roboat = require('../lib/roboat');
 
+if(process.env.BOT_API_KEY == null)
+	throw new Error("BOT_API_KEY not set");
+	
 var token = process.env.BOT_API_KEY.trim();
 var name = process.env.BOT_NAME;
+
 
 var roboat = new Roboat({
 	token: token,
